94c2dc5ef47da39f8bf9f1276b9748073151b787
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-24 08:43:09 +0300

    Add flowbite for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: ['node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}']
   },
-  plugins: []
+  plugins: [require('flowbite/plugin')]
 };
